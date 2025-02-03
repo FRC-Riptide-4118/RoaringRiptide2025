cbbfecfa9619eccc4ab4eb06ff767a5bc5413123
andrei2O28 <royalspooder456@gmail.com>
2025-02-02 19:21:17 -0500

    Worked on mech 2d
    changes at home

--- src/main/java/frc/robot/subsystems/simulation/SimulationViewer.java
+++ src/main/java/frc/robot/subsystems/simulation/SimulationViewer.java
@@ -1,5 +1,9 @@
 package frc.robot.subsystems.simulation;
 
+import edu.wpi.first.wpilibj.AnalogPotentiometer;
+import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
 import edu.wpi.first.wpilibj.smartdashboard.Mechanism2d;
 import edu.wpi.first.wpilibj.smartdashboard.MechanismLigament2d;
 import edu.wpi.first.wpilibj.smartdashboard.MechanismRoot2d;
@@ -12,9 +16,14 @@ import java.util.function.DoubleSupplier;
 public class SimulationViewer extends SubsystemBase {
   private static final double kElevatorMinimumLength = 0.5;
 
+  private final PWMSparkMax m_wristMotor = new PWMSparkMax(1);
+  private final PWMSparkMax m_elevatorMotor = new PWMSparkMax(0);
   private final MechanismLigament2d m_elevator;
   private final MechanismLigament2d m_wrist;
   private final DoubleSupplier doubleSupplier;
+  private final Encoder m_elevatorEncoder = new Encoder(0, 1);
+  private final AnalogPotentiometer m_wristPot = new AnalogPotentiometer(1, 90);
+  private final Joystick m_joystick = new Joystick(0);
 
   public SimulationViewer(DoubleSupplier doubleSupplier) {
     this.doubleSupplier = doubleSupplier;
@@ -35,5 +44,13 @@ public class SimulationViewer extends SubsystemBase {
   }
 
   @Override
-  public void periodic() {}
+  public void periodic() {
+    // if there are bugs, change this to robot periodic
+    m_elevator.setLength(kElevatorMinimumLength + m_elevatorEncoder.getDistance());
+    m_wrist.setAngle(m_wristPot.get());
+
+    // if there are bugs, change this to teleop periodic
+    m_elevatorMotor.set(m_joystick.getRawAxis(0));
+    m_wristMotor.set(m_joystick.getRawAxis(1));
+  }
 }
