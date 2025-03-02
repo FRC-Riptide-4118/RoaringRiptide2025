1ec292fbcadd46b4037a4297523adf37d955aefb
andrei2O28 <royalspooder456@gmail.com>
2025-03-02 15:54:29 -0500

    adding algae intake to simulation viewer

--- src/main/java/frc/robot/subsystems/simulation/SimulationViewer.java
+++ src/main/java/frc/robot/subsystems/simulation/SimulationViewer.java
@@ -9,13 +9,18 @@ import org.littletonrobotics.junction.mechanism.LoggedMechanismRoot2d;
 
 public class SimulationViewer extends SubsystemBase {
   private static final double kElevatorMinimumLength = 0.5;
+  private static final double kAlgaeIntakeRadius = 0.5; // this value is a guess, change later
 
   private final LoggedMechanismLigament2d m_elevator;
+  private final LoggedMechanismLigament2d m_algaeIntake;
   // private final MechanismLigament2d m_wrist;
   private final DoubleSupplier elevatorPosSupplier;
+  private final DoubleSupplier algaeIntakeSpeedSupplier;
 
-  public SimulationViewer(DoubleSupplier doubleSupplier) {
-    this.elevatorPosSupplier = doubleSupplier;
+  public SimulationViewer(
+      DoubleSupplier elevatorPosSupplier, DoubleSupplier algaeIntakeSpeedSupplier) {
+    this.elevatorPosSupplier = elevatorPosSupplier;
+    this.algaeIntakeSpeedSupplier = algaeIntakeSpeedSupplier;
     // the main mechanism object
     LoggedMechanism2d mech = new LoggedMechanism2d(3, 3);
     // the mechanism root node
@@ -24,20 +29,23 @@ public class SimulationViewer extends SubsystemBase {
     // MechanismLigament2d objects represent each "section"/"stage" of the mechanism, and are based
     // off the root node or another ligament object
     m_elevator = root.append(new LoggedMechanismLigament2d("elevator", kElevatorMinimumLength, 90));
+    m_algaeIntake =
+        m_elevator.append(new LoggedMechanismLigament2d("Algae Intake", kAlgaeIntakeRadius, 0));
     // m_wrist =
     //     m_elevator.append(
     //         new MechanismLigament2d("wrist", 0.5, 90, 6, new Color8Bit(Color.kPurple)));
 
     // post the mechanism to the dashboard
-    Logger.recordOutput("elevator", mech);
+    Logger.recordOutput("Simulation viewer", mech);
   }
 
   @Override
   public void periodic() {
     // if there are bugs, change this to robot periodic
     double elevatorPosition = elevatorPosSupplier.getAsDouble();
+    double algaeIntakeSpeed = algaeIntakeSpeedSupplier.getAsDouble();
     m_elevator.setLength(kElevatorMinimumLength + elevatorPosition);
-    // m_wrist.setAngle(m_wristPot.get());
-
+    m_algaeIntake.setAngle(algaeIntakeSpeed * 1);
+    // m_wrist.setAngle(m_wristPot.get())
   }
 }
